86a08b31a8ac270f804d3d2d35a20eac6d48e7d0
Valentine101 <74081251+Valentine101@users.noreply.github.com>
2021-10-15 15:13:43 -0400

    tutorial about tokens given from Google https://www.youtube.com/watch?v=5-jBzkttbx0&list=PL06B5SYY5oUKyps79g7Ud96whBVL03gaX&index=4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import GoogleLogin from "react-google-login";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  //use state variables
+  const [name, setName] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  const [url, setUrl] = useState("");
+
+  const responseGoogle = (response) => {
+    setName(response.profileObj.name);
+    setEmail(response.profileObj.email);
+    setUrl(response.profileObj.imageUrl);
+    console.log(response);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Login w/ Google</h1>
+      <h2>Welcome: {name}</h2>
+      <h2>Email: {email}</h2>
+      <h2>
+        Profile Pic: <img src={url} alt={name} />{" "}
+      </h2>
+      <br />
+      <GoogleLogin
+        clientId="136092104401-jorrpuda1pbbmo2c24ko1fr6rrl3s6cc.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
 }
